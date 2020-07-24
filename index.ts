dfff9230d7784f9519dc1aa27cc045387284794e
Anand Chowdhary <mail@anandchowdhary.com>
2020-07-24 13:59:18 +0530

    :construction: Add basic responder

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
 import polka from "polka";
+const send = require("@polka/send-type");
+
+const server = polka().all("/", (_, response) => {
+  console.log("OK");
+  return send(response, 200, {
+    text: "Okay, I'm triggering a build on Netlify 🚀",
+  });
+});
+
+(server as any).listen(3003, (error: any) => {
+  if (error) console.log(error);
+  console.log(`> Running on localhost:3003`);
+});
