736e5bddf8a21df230e4253f568e4b0cbd399c2e
Anand Chowdhary <mail@anandchowdhary.com>
2020-07-24 14:21:54 +0530

    :sparkles: Trigger Netlify build webhook

--- index.ts
+++ index.ts
@@ -1,11 +1,22 @@
 const polka = require("polka");
 const send = require("@polka/send-type");
+import axios from "axios";
 
-const server = polka().all("/", (_: any, response: any) => {
-  console.log("OK");
-  return send(response, 200, {
-    text: "Okay, I'm triggering a build on Netlify 🚀",
-  });
+const server = polka().post("/", (_: any, response: any) => {
+  console.log("Triggering Netlify build", new Date());
+  axios
+    .post(process.env.NETLIFY_WEBHOOK ?? "")
+    .then(() =>
+      send(response, 200, {
+        text: "Okay, I'm triggering a build on Netlify 🚀",
+      })
+    )
+    .catch((error) => {
+      console.log(error);
+      send(response, 200, {
+        text: "I got an error in trying to deploy the site.",
+      });
+    });
 });
 
 (server as any).listen(process.env.PORT ?? 3003, (error: any) => {
